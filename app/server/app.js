c3684d397d0fe6dc29689fa781a2631c4a84d40b
Evan Chen <evanchen7@yahoo.com>
2018-01-03 13:42:38 -0800

    'helmet package'

--- app/server/app.js
+++ app/server/app.js
@@ -1,5 +1,7 @@
+'use strict';
 import path from 'path';
 import express from 'express';
+import helmet from 'helmet';
 // import router from './router';
 
 const app = express();
@@ -9,9 +11,14 @@ const indexPath = express.static(path.join(__dirname, '../index.html'));
 
 // Middleware
 app.use(publicPath);
+app.use(helmet());
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   res.sendFile(indexPath);
+});
+
+app.get('/health-check', (req, res) => {
+  res.sendStatus(200);
 });
 
 export default app;
