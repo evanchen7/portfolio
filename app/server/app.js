12183b7e6090e9403b5c2c414ca080255a342a21
Evan Chen <evanchen7@yahoo.com>
2018-02-02 16:28:23 -0800

    'added bodyParser and a test endpoint'

--- app/server/app.js
+++ app/server/app.js
@@ -2,6 +2,8 @@
 import path from 'path';
 import express from 'express';
 import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
 // import router from './router';
 
 const app = express();
@@ -10,9 +12,11 @@ const publicPath = express.static(path.join(__dirname, '../'));
 const static1 = express.static('static');
 
 // Middleware
- app.use(helmet());
+app.use(helmet());
 app.use(publicPath);
 app.use(static1);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(indexPath);
@@ -20,6 +24,15 @@ app.get('/', (req, res) => {
 
 app.get('/health-check', (req, res) => {
   res.sendStatus(200);
+});
+
+app.post('/test', (req, res) => {
+  let x = parseInt(req.body.x), y = parseInt(req.body.y);
+  if (isNaN(x) || isNaN(y)) res.send('Send a valid number for x or y value and try again');
+  let sum = {
+    "sum": x + y
+  };
+  res.send(sum);
 });
 
 export default app;
